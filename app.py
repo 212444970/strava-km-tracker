5eef6efd2de1f576e7f03dabfcb0151ce5a4eef1
212444970 <56927600+212444970@users.noreply.github.com>
2026-08-23 21:31:52 +0200

    Show garmin_archive status on admin page

--- app.py
+++ app.py
@@ -70,6 +70,7 @@ def admin():
         "admin.html",
         garmin_connected=garmin_client.is_connected(),
         archive_count=len(strava_archive_store.load()),
+        garmin_archive_count=len(garmin_archive_store.load()),
     )
 
 
@@ -85,11 +86,13 @@ def admin_garmin_login():
         return render_template("admin.html",
                                garmin_connected=False,
                                archive_count=len(strava_archive_store.load()),
+                               garmin_archive_count=len(garmin_archive_store.load()),
                                needs_mfa=True)
     _, msg = result
     return render_template("admin.html",
                            garmin_connected=False,
                            archive_count=len(strava_archive_store.load()),
+                           garmin_archive_count=len(garmin_archive_store.load()),
                            garmin_error=msg)
 
 
@@ -104,6 +107,7 @@ def admin_garmin_mfa():
     return render_template("admin.html",
                            garmin_connected=False,
                            archive_count=len(strava_archive_store.load()),
+                           garmin_archive_count=len(garmin_archive_store.load()),
                            garmin_error=msg)
 
 
